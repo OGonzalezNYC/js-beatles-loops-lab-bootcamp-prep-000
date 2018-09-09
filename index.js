ae92be0d4f8400eac3f03076cc4b447509518570
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-09 19:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function theBeatlesPlay(musicians_array, instruments_array) {
   var array = [];
   for  (let index = 0; index < musicians_array.length; index++) {
     array[index] = `${musicians_array[index]} plays ${instruments_array[index]}`
-=======
-// add solution here
-function theBeatlesPlay(musicians_array, instruments_array) {
-  //array = []
-  for  (let index = 0; index < musicians_array.length; index++) {
-    array = [];
-    array[index] = `${[musicians_array][index]} ${[instruments_array][index]`
->>>>>>> b13172f9c97ddd93f74d006ad1238e46f87f2f7b
   }
   return array
 }
